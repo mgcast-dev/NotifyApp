1527d0a396074092d2a26432ccb37397e89d9559
Marcgom-upf <marc.gomez09@estudiant.upf.edu>
2026-02-24 02:38:37 +0100

    Commit inicial: Creación del proyecto React Native

--- App.tsx
+++ App.tsx
@@ -1,44 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
+const App = () => {
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={styles.text}>¡SISTEMA ACTIVO!</Text>
+      <Text style={styles.subtext}>Si ves esto, el puente funciona.</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#000',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#0f0',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  subtext: {
+    color: '#fff',
+    marginTop: 10,
   },
 });
 
