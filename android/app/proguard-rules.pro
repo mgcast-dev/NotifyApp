58801f8d97fbd67c47cb1e52911b549e0327a318
mgcast-dev <marcgomez2006@gmail.com>
2026-08-23 19:59:00 +0200

    Error: app crushes in release

--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -8,3 +8,16 @@
 #   http://developer.android.com/guide/developing/tools/proguard.html
 
 # Add any project specific keep options here:
+# Mantener el puente nativo de NotifyApp a salvo de la ofuscación
+-keep class com.notifyapp.PermissionsModule { *; }
+-keep class com.notifyapp.PermissionsPackage { *; }
+
+# Evitar que ProGuard/R8 ofusque o elimine nuestro código nativo y el puente
+-keep class com.notifyapp.** { *; }
+-keepclassmembers class com.notifyapp.** { *; }
+
+# Mantener los métodos anotados de React Native intactos
+-keepattributes *Annotation*,Signature,InnerClasses
+-keepclassmembers class * {
+    @com.facebook.react.bridge.ReactMethod *;
+}
--- android/app/src/main/java/com/notifyapp/MainApplication.kt
+++ android/app/src/main/java/com/notifyapp/MainApplication.kt
@@ -4,20 +4,23 @@ import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactPackage
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.runtime.JSCInstance // <--- IMPORTANTE: Añade este import para el motor JSC
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactHost: ReactHost by lazy {
+    val packages: MutableList<ReactPackage> = PackageList(this).packages.toMutableList()
+    packages.add(PermissionsPackage())
+
+    // Pasamos los parámetros exactos que nos pide la firma,
+    // inyectando JSCInstance() para apagar Hermes nativamente.
     getDefaultReactHost(
       context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-          add(PermissionsPackage())
-        },
+      packageList = packages,
+      jsRuntimeFactory = JSCInstance() // <--- Aquí le decimos al Host: "Usa JavaScriptCore nativo"
     )
   }
 
